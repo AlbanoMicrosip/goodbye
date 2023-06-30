70109661acdc502d0d63a0189d013ccfb27dae17
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-06-30 12:11:14 -0600

    Cambiando a programacion reactiva

--- src/main/java/com/goodbye/goodbye/GoodbyeController.java
+++ src/main/java/com/goodbye/goodbye/GoodbyeController.java
@@ -1,18 +1,25 @@
 package com.goodbye.goodbye;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class GoodbyeController {
-  @Autowired
-  private RestTemplate restTemplate;
+  private final WebClient.Builder webClientBuilder;
+
+  public GoodbyeController(WebClient.Builder webClientBuilder) {
+    this.webClientBuilder = webClientBuilder;
+  }
 
   @GetMapping("/goodbye")
-  public String goodbye() {
-    String response = restTemplate.getForObject("http://say-hello/hello", String.class);
-    return "Goodbye. " + response;
+  public Mono<String> goodbye() {
+    return webClientBuilder.build()
+      .get()
+      .uri("http://say-hello/hello")
+      .retrieve()
+      .bodyToMono(String.class)
+      .map(response -> "Goodbye. " + response);
   }
 }
