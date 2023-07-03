7cce75f8a635c0f56ae9c61610a2fc27889d100e
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-07-03 15:07:23 -0600

    Activando HazelcastConfig

--- src/main/java/com/goodbye/goodbye/HazelcastConfig.java
+++ src/main/java/com/goodbye/goodbye/HazelcastConfig.java
@@ -2,6 +2,7 @@ package com.goodbye.goodbye;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.DiscoveryStrategyConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.ManagedContext;
 import com.hazelcast.eureka.one.EurekaOneDiscoveryStrategyFactory;
@@ -14,18 +15,26 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class HazelcastConfig {
 
   @Bean
-  public ClientConfig clientConfig() {
+  public ClientConfig hazelcastClientConfig() {
     ClientConfig clientConfig = new ClientConfig();
-    clientConfig.getNetworkConfig().getEurekaConfig()
-      .setEnabled(true)
-      .setProperty("self-registration", "true")
-      .setProperty("namespace", "hazelcast");
 
-//    clientConfig.getGroupConfig().setName("HAZELCAST-SAY"); // reemplaza "hazelcast-say" con el nombre de tu grupo Hazelcast si es diferente
+    clientConfig.setProperty("hazelcast.discovery.enabled", "true");
+    EurekaOneDiscoveryStrategyFactory discoveryStrategyFactory = new EurekaOneDiscoveryStrategyFactory();
+    Map<String, Comparable> properties = new HashMap<>();
+    properties.put("self-registration", "true");
+    properties.put("namespace", "hazelcast");
+    DiscoveryStrategyConfig discoveryStrategyConfig = new DiscoveryStrategyConfig(discoveryStrategyFactory, properties);
+
+    clientConfig.getNetworkConfig().getDiscoveryConfig()
+      .addDiscoveryStrategyConfig(discoveryStrategyConfig);
+
     return clientConfig;
   }
 
