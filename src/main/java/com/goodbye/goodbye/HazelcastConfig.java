9f44e7414b52911231ba7d6e7371875f260ff25e
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-07-03 14:30:48 -0600

    Registrar ocn eureka

--- src/main/java/com/goodbye/goodbye/HazelcastConfig.java
+++ src/main/java/com/goodbye/goodbye/HazelcastConfig.java
@@ -1,47 +1,47 @@
-package com.goodbye.goodbye;
-
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.ManagedContext;
-import com.hazelcast.eureka.one.EurekaOneDiscoveryStrategyFactory;
-import com.hazelcast.spring.context.SpringManagedContext;
-import com.netflix.discovery.EurekaClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class HazelcastConfig {
-
-  private static Logger log = LoggerFactory.getLogger(HazelcastConfig.class);
-
-  private ApplicationContext applicationContext;
-
-  @Bean
-  public ManagedContext managedContext() {
-    ManagedContext springManagedContext = new SpringManagedContext();
-    return springManagedContext;
-  }
-  @Bean
-  public ClientConfig clientConfig() {
-    ClientConfig clientConfig = new ClientConfig();
-    clientConfig.getNetworkConfig().getEurekaConfig()
-      .setEnabled(true)
-      .setProperty("self-registration", "true")
-      .setProperty("namespace", "hazelcast");
-
-    clientConfig.getGroupConfig().setName("HAZELCAST-SAY"); // reemplaza "hazelcast-say" con el nombre de tu grupo Hazelcast si es diferente
-    return clientConfig;
-  }
-
-
-  @Bean
-  public HazelcastInstance hazelcastInstance(ClientConfig clientConfig) {
-    HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
-    return client;
-  }
-}
+//package com.goodbye.goodbye;
+//
+//import com.hazelcast.client.HazelcastClient;
+//import com.hazelcast.client.config.ClientConfig;
+//import com.hazelcast.core.HazelcastInstance;
+//import com.hazelcast.core.ManagedContext;
+//import com.hazelcast.eureka.one.EurekaOneDiscoveryStrategyFactory;
+//import com.hazelcast.spring.context.SpringManagedContext;
+//import com.netflix.discovery.EurekaClient;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class HazelcastConfig {
+//
+//  private static Logger log = LoggerFactory.getLogger(HazelcastConfig.class);
+//
+//  private ApplicationContext applicationContext;
+//
+//  @Bean
+//  public ManagedContext managedContext() {
+//    ManagedContext springManagedContext = new SpringManagedContext();
+//    return springManagedContext;
+//  }
+//  @Bean
+//  public ClientConfig clientConfig() {
+//    ClientConfig clientConfig = new ClientConfig();
+//    clientConfig.getNetworkConfig().getEurekaConfig()
+//      .setEnabled(true)
+//      .setProperty("self-registration", "true")
+//      .setProperty("namespace", "hazelcast");
+//
+//    clientConfig.getGroupConfig().setName("HAZELCAST-SAY"); // reemplaza "hazelcast-say" con el nombre de tu grupo Hazelcast si es diferente
+//    return clientConfig;
+//  }
+//
+//
+//  @Bean
+//  public HazelcastInstance hazelcastInstance(ClientConfig clientConfig) {
+//    HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+//    return client;
+//  }
+//}
