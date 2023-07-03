e3dc383d3beb825864188f265073a59a69f921d2
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-07-03 14:48:50 -0600

    Activando HazelcastConfig

--- src/main/java/com/goodbye/goodbye/HazelcastConfig.java
+++ src/main/java/com/goodbye/goodbye/HazelcastConfig.java
@@ -1,47 +1,53 @@
-//package com.goodbye.goodbye;
-//
-//import com.hazelcast.client.HazelcastClient;
-//import com.hazelcast.client.config.ClientConfig;
-//import com.hazelcast.core.HazelcastInstance;
-//import com.hazelcast.core.ManagedContext;
-//import com.hazelcast.eureka.one.EurekaOneDiscoveryStrategyFactory;
-//import com.hazelcast.spring.context.SpringManagedContext;
-//import com.netflix.discovery.EurekaClient;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class HazelcastConfig {
-//
-//  private static Logger log = LoggerFactory.getLogger(HazelcastConfig.class);
-//
-//  private ApplicationContext applicationContext;
-//
-//  @Bean
-//  public ManagedContext managedContext() {
-//    ManagedContext springManagedContext = new SpringManagedContext();
-//    return springManagedContext;
-//  }
-//  @Bean
-//  public ClientConfig clientConfig() {
-//    ClientConfig clientConfig = new ClientConfig();
-//    clientConfig.getNetworkConfig().getEurekaConfig()
-//      .setEnabled(true)
-//      .setProperty("self-registration", "true")
-//      .setProperty("namespace", "hazelcast");
-//
-//    clientConfig.getGroupConfig().setName("HAZELCAST-SAY"); // reemplaza "hazelcast-say" con el nombre de tu grupo Hazelcast si es diferente
-//    return clientConfig;
-//  }
-//
-//
-//  @Bean
-//  public HazelcastInstance hazelcastInstance(ClientConfig clientConfig) {
-//    HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
-//    return client;
-//  }
-//}
+package com.goodbye.goodbye;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.DiscoveryStrategyConfig;
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ManagedContext;
+import com.hazelcast.eureka.one.EurekaOneDiscoveryStrategyFactory;
+import com.hazelcast.spring.context.SpringManagedContext;
+import com.netflix.discovery.EurekaClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class HazelcastConfig {
+
+  @Value("${hazelcast.port:5701}")
+  private int hazelcastPort;
+
+  @Bean
+  public Config configLocal() {
+    Config config = new Config();
+    config.getNetworkConfig().setPort(hazelcastPort);
+    config.getProperties().setProperty("hazelcast.discovery.enabled", "true");
+    JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+    joinConfig.getMulticastConfig().setEnabled(false);
+    EurekaOneDiscoveryStrategyFactory discoveryStrategyFactory = new EurekaOneDiscoveryStrategyFactory();
+    Map<String, Comparable> properties = new HashMap<>();
+    properties.put("self-registration", "true");
+    properties.put("namespace", "hazelcast");
+    DiscoveryStrategyConfig discoveryStrategyConfig = new DiscoveryStrategyConfig(discoveryStrategyFactory, properties);
+    joinConfig.getDiscoveryConfig().addDiscoveryStrategyConfig(discoveryStrategyConfig);
+
+    return config;
+  }
+
+
+  @Bean
+  public HazelcastInstance hazelcastInstance(ClientConfig clientConfig) {
+    HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+    return client;
+  }
+}
