86b6f6faa8e97fa63f767b53f76057f4e3493323
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-07-03 15:28:42 -0600

    aaa

--- src/main/java/com/goodbye/goodbye/AppConfig.java
+++ src/main/java/com/goodbye/goodbye/AppConfig.java
@@ -1,14 +1,14 @@
-package com.goodbye.goodbye;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
-
-@Configuration
-public class AppConfig {
-
-  @Bean
-  public WebClient.Builder webClientBuilder() {
-    return WebClient.builder();
-  }
-}
+//package com.goodbye.goodbye;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.reactive.function.client.WebClient;
+//
+//@Configuration
+//public class AppConfig {
+//
+//  @Bean
+//  public WebClient.Builder webClientBuilder() {
+//    return WebClient.builder();
+//  }
+//}
