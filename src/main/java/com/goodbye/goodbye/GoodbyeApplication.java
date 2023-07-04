3aae7abed0fbb9cf17c4c560171e3df7afb80319
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-07-04 11:59:40 -0600

    Tratando de registrar instancia de Servicio en Eureka a la ves que se conecta a hazelcast

--- src/main/java/com/goodbye/goodbye/GoodbyeApplication.java
+++ src/main/java/com/goodbye/goodbye/GoodbyeApplication.java
@@ -1,18 +1,41 @@
 package com.goodbye.goodbye;
 
 import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.ClientUserCodeDeploymentConfig;
 import com.hazelcast.config.DiscoveryStrategyConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.instance.HazelcastInstanceFactory;
+import com.hazelcast.spring.context.SpringManagedContext;
+import com.hazelcast.core.ManagedContext;
 import com.hazelcast.eureka.one.EurekaOneDiscoveryStrategyFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.PreDestroy;
 import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootApplication
-public class GoodbyeApplication {
+public class GoodbyeApplication implements ApplicationContextAware {
+
+	@Bean
+	public ManagedContext managedContext() {
+		ManagedContext springManagedContext = new SpringManagedContext();
+		return springManagedContext;
+	}
+
+	private ApplicationContext applicationContext;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
 
 	@Value("${hazelcast.port:5701}")
 	private int hazelcastPort;
@@ -20,10 +43,8 @@ public class GoodbyeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GoodbyeApplication.class, args);
 	}
-
-
 	@Bean
-	public ClientConfig hazelcastConfig() {
+	public ClientConfig hazelcastConfig(ManagedContext managedContext) {
 		ClientConfig clientConfig = new ClientConfig();
 		clientConfig.setProperty("hazelcast.discovery.enabled", "true");
 
@@ -35,6 +56,18 @@ public class GoodbyeApplication {
 		clientConfig.getNetworkConfig().getDiscoveryConfig()
 			.addDiscoveryStrategyConfig(discoveryStrategyConfig);
 
+		clientConfig.setManagedContext(managedContext);
+
+		ClientUserCodeDeploymentConfig clientUserCodeDeploymentConfig = new ClientUserCodeDeploymentConfig();
+		clientUserCodeDeploymentConfig.setEnabled(true);
+		clientConfig.setUserCodeDeploymentConfig(clientUserCodeDeploymentConfig);
+
 		return clientConfig;
+	}
+
+	@PreDestroy
+	void destroyHazelcast() {
+		Hazelcast.shutdownAll();
+		HazelcastInstanceFactory.shutdownAll();
 	}
 }
