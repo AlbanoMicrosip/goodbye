7241204bae151e9715cfea3bc6204c8c11945f2b
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-07-03 23:16:40 -0600

    Probando Hazelcast

--- src/main/java/com/goodbye/goodbye/GoodbyeApplication.java
+++ src/main/java/com/goodbye/goodbye/GoodbyeApplication.java
@@ -1,17 +1,31 @@
 package com.goodbye.goodbye;
 
+import com.hazelcast.client.config.ClientConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GoodbyeApplication {
+
+	@Value("${hazelcast.port:5701}")
+	private int hazelcastPort;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoodbyeApplication.class, args);
 	}
 
 
+	@Bean
+	public ClientConfig hazelcastConfig() {
+		ClientConfig clientConfig = new ClientConfig();
+		clientConfig.getNetworkConfig().getEurekaConfig().setEnabled(true)
+			.setProperty("namespace", "hazelcast");
+
+		return clientConfig;
+	}
 }
