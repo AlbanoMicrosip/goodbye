849cb993bec10796a33341d1ac30d031578b66ed
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-07-05 17:09:29 -0600

    Probando Spring cloud load balancer

--- src/main/java/com/goodbye/goodbye/GoodbyeController.java
+++ src/main/java/com/goodbye/goodbye/GoodbyeController.java
@@ -1,9 +1,6 @@
 package com.goodbye.goodbye;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -12,19 +9,16 @@ import org.springframework.web.client.RestTemplate;
 public class GoodbyeController {
 
   private final RestTemplate restTemplate;
-  private final LoadBalancerClient loadBalancerClient;
 
   @Autowired
-  public GoodbyeController(RestTemplate restTemplate, LoadBalancerClient loadBalancerClient) {
+  public GoodbyeController(RestTemplate restTemplate) {
     this.restTemplate = restTemplate;
-    this.loadBalancerClient = loadBalancerClient;
   }
 
   @GetMapping("/call-say-instance")
   public String callSayInstance() {
-    ServiceInstance instance = loadBalancerClient.choose("SAY-INSTANCE");
-    String url = instance.getUri().toString();
-    return restTemplate.getForObject(url, String.class);
+    System.out.println("Viendo si soy llamdo");
+    return restTemplate.getForObject("http://" + "SAY-INSTANCE" + "/", String.class);
   }
   @GetMapping("/call-say-instance-fix")
   public String callSayInstanceFix() {
