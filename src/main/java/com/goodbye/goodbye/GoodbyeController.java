692bdc5300631858754d48574460135bdc0ede3b
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-07-05 16:00:03 -0600

    Peticiones a Say-hello

--- src/main/java/com/goodbye/goodbye/GoodbyeController.java
+++ src/main/java/com/goodbye/goodbye/GoodbyeController.java
@@ -3,8 +3,10 @@ package com.goodbye.goodbye;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@RestController
 public class GoodbyeController {
 
   private final RestTemplate restTemplate;
