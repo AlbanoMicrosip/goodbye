4dfd3e7df87571e4c03be37b6f8e8fcce081c702
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-08-21 16:06:43 -0600

    Viendo si cachamos sigterm

--- src/main/java/com/goodbye/goodbye/GoodbyeApplication.java
+++ src/main/java/com/goodbye/goodbye/GoodbyeApplication.java
@@ -3,21 +3,21 @@ package com.goodbye.goodbye;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import sun.misc.Signal;
+
 @SpringBootApplication
 public class GoodbyeApplication {
 	public static void main(String[] args) {
+
+		// Manejo de la señal SIGTERM
+		Signal.handle(new Signal("TERM"), signal -> {
+			System.out.println("Se recibió SIGTERM. Limpiando y saliendo...");
+			// Aquí puedes agregar cualquier lógica de limpieza que necesites
+			System.exit(0);
+		});
+
+
 		SpringApplication.run(GoodbyeApplication.class, args);
 	}
 
-//	@Bean
-//	@LoadBalanced
-//	public RestTemplate restTemplate() {
-//		return new RestTemplate();
-//	}
-//
-//	@Bean
-//	@Qualifier("NoBalancing")
-//	public RestTemplate restTemplateNoBalancing() {
-//		return new RestTemplate();
-//	}
 }
